8bfa6ae4703593453d86372d6c7b6da6a3a54f64
jmoone16 <jmoone16@wgu.edu>
2026-08-24 07:27:19 -0600

    Add student find by id example

--- 04-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
+++ 04-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
@@ -24,4 +24,10 @@ public class StudentDAOImpl implements StudentDAO {
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
     }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+        // takes the class and id of the object it's looking for.
+    }
 }
